f7e77c9b278896601a42904e9ac12e1e0eb046c8
Radhika Nikam <radhika23594@gmail.com>
2018-01-30 04:16:31 +0800

    added kappa_per_thread

--- scripts/analysis/fleiss_kappa_per_thread.py
+++ scripts/analysis/fleiss_kappa_per_thread.py
@@ -5,47 +5,42 @@ import csv
 import math
 from krippendorff import *
 from statsmodels.stats.inter_rater import aggregate_raters
-import mysql.connector,sqlite3 
+import sqlite3 
 
 class fleiss_kappa:
-	def __init__(self,data):
-		self.data = data
+    def __init__(self,data):
+    	self.data = data
 
-	def calc_fleiss_kappa(self):
-		
-		#From http://www.statsmodels.org/dev/stats.html
-	
-		table = 1.0 * np.asarray(self.data)   #avoid integer division
-		self.n_sub, self.n_cat =  table.shape
-		self.n_total = table.sum()
-		self.n_rater = table.sum(1)
-		self.n_rat = self.n_rater.max()
-		assert self.n_total == self.n_sub * self.n_rat
-		self.p_cat = table.sum(0) / self.n_total
+    def calc_fleiss_kappa(self):		
+	    """
+        From http://www.statsmodels.org/dev/stats.html
+        """
+	    table = 1.0 * np.asarray(self.data)   #avoid integer division
+	    self.n_sub, self.n_cat =  table.shape
+	    self.n_total = table.sum()
+	    self.n_rater = table.sum(1)
+	    self.n_rat = self.n_rater.max()
+	    assert self.n_total == self.n_sub * self.n_rat
+	    self.p_cat = table.sum(0) / self.n_total
 
-		self.table2 = table * table
-		self.p_rat = (self.table2.sum(1) - self.n_rat) / (self.n_rat *
-                        (self.n_rat - 1))
-		self.p_mean = self.p_rat.mean()
-
-		self.p_mean_exp = (self.p_cat*self.p_cat).sum()
-
-		kappa = (self.p_mean - self.p_mean_exp) / (1- self.p_mean_exp)
-		
-		return kappa
-
-	def calc_std_dev(self):
-		
-#From https://i1.wp.com/www.real-statistics.com/wp-content/uploads/2013/11/image102c.png
-		
-		self.var_num_1 = (self.p_mean_exp - (2*self.n_rater-3)*(
-			self.p_mean_exp)**2)
-		self.var_num_2 = 2*(self.n_rater-2)*(self.p_cat**3).sum()
-		self.var_num = 2*(self.var_num_1+self.var_num_2)
-		self.var_den = self.n_rater*self.n_sub*(self.n_rater-1)*(1-
-			self.p_mean_exp)**2
-		var = self.var_num/self.var_den
-		return math.sqrt(var[0])
+	    self.table2 = table * table
+	    self.p_rat = (self.table2.sum(1) - self.n_rat) / (self.n_rat*(self.n_rat - 1))
+	    self.p_mean = self.p_rat.mean()
+	    self.p_mean_exp = (self.p_cat*self.p_cat).sum()
+	    kappa = (self.p_mean - self.p_mean_exp) / (1- self.p_mean_exp)	
+	    return kappa
+    
+    def calc_std_dev(self):
+	        #From https://i1.wp.com/www.real-statistics.com/wp-content/uploads
+            #/2013/11/image102c.png	
+        
+        self.var_num_1 = (self.p_mean_exp - (2*self.n_rater-3)*(self.p_mean_exp)**2)
+        self.var_num_2 = 2*(self.n_rater-2)*(self.p_cat**3).sum()
+        self.var_num = 2*(self.var_num_1+self.var_num_2)
+        self.var_den = self.n_rater*self.n_sub*(self.n_rater-1)*(1-
+        	self.p_mean_exp)**2
+        var = self.var_num/self.var_den
+        return math.sqrt(var[0])
 
 data = pd.read_csv('warhol_task1.1.csv')
 data = data.loc[:,'Input.threadtitle':'Answer.noreply']
@@ -59,12 +54,16 @@ for title in titles:
     df = df.replace('none',1)
     conn = sqlite3.connect('cs6207.db')
     c = conn.cursor()
-    c.execute('select thread_id from post2 inner join thread on post2.thread_id= thread.id where original=1 and post2.courseid like "%%warhol%%" and thread.title like '+'"%%'+title+'%%"') 
+    c.execute('select thread_id from post2 inner join thread on post2.thread_id=\
+            thread.id where original=1 and post2.courseid like "%%warhol%%" and \
+            thread.title like '+'"%%'+title+'%%"') 
     thread_id = c.fetchone()
-    c.execute("select count(1) from post2 where thread_id=? and courseid like '%warhol%'",
+    c.execute("select count(1) from post2 where thread_id=? and courseid like \
+            '%warhol%'",
             thread_id)
     post2 = c.fetchone()
-    c.execute("select count(1) from comment2 where thread_id=? and courseid like '%warhol%'", thread_id)
+    c.execute("select count(1) from comment2 where thread_id=? and courseid like  \
+            '%warhol%'", thread_id)
     comment2 = c.fetchone()
     length = post2[0]+comment2[0] 
     print(length)
@@ -75,8 +74,6 @@ for title in titles:
             df1['Answer.'+ str(i+1)]  = df['Answer.'+ str(i+1)] 
         except:
             pass
-       
-    
     df1['Answer.noreply'] = df['Answer.noreply']
     aggregate = aggregate_raters(df1.T)
 
@@ -87,24 +84,4 @@ for title in titles:
     
 print("\nAverage Kappa:"+str(np.mean(fks)))
 print("Std Dev:" + str(np.std(fks)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
