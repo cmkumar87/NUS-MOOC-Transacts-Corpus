aacf3037c8828696f1bce4d7674b3e0d32ee9b77
cmkumar87 <cmkumar087@gmail.com>
2018-11-29 22:52:11 +0800

    bug fixes utf8 read; sql query fix with trim; other string normalisations

--- scripts/baselines/bin/cleanup_data.py
+++ scripts/baselines/bin/cleanup_data.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 import re
+#from __future__ import unicode_literals
 
 '''
 This script cleans the text in the annotated corpus. It substitutes equations, time and links with #MATH, #URLREF and #TIMEREF
@@ -8,7 +9,9 @@ This script cleans the text in the annotated corpus. It substitutes equations, t
 '''
 
 
-courses_df = pd.read_csv('../data/nustotal.csv', encoding = "latin1")
+#courses_df = pd.read_csv('../data/nustotal.csv', encoding = "latin1")
+courses_df = pd.read_csv('../data/organalysis-003_out_.csv', encoding = "utf8")
+
 thread_titles = courses_df['Title']
 courses = courses_df['Course']
 del courses_df['post']
@@ -19,16 +22,17 @@ conn.text_factory = lambda x: str(x, 'latin1')
 c = conn.cursor()
 
 for thread_title in thread_titles:
+    thread_title = thread_title.strip()
+    courseid = courses_df.loc[courses_df['Title']==thread_title,'Course'].iloc[0].strip()
     thread_text = ''
     thread_comment_text = ''
-    post_count=0
-    comment_count =0
+    post_count = 0
+    comment_count = 0
     for row in c.execute('select thread.id, post2.id, post2.post_text from post2 inner join thread on \
-        post2.thread_id=thread.id where thread.title=? and post2.courseid=?  order by post2.post_order ASC',
-        (thread_title,courses_df.loc[courses_df['Title']==thread_title,'Course'].iloc[0])):
+        post2.thread_id=thread.id where trim(thread.title)=? and post2.courseid=?  order by post2.post_order ASC',
+        (thread_title,courseid)):
         post_count+=1
         thread_id,post_id,post_text = row
-
         post_text = re.sub(r'(https?://)?(www\.)?([a-zA-Z0-9_%]*)\b\.[a-z]{2,4}(\.[a-z]{2})?((/[a-zA-Z0-9_%]*)+)?(\.[a-z]*)?','#URLREF',post_text)
         post_text = re.sub(r'([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?','#TIMEREF',post_text)
         post_text = re.sub(r'(\()+.*[+*/-]+.*(\))+','#MATH',post_text)
@@ -62,5 +66,7 @@ for thread_title in thread_titles:
 # REGEX
 '''
 
-courses_df.to_csv('../data/nustotal_preprocessed.csv')
+#courses_df.to_csv('../data/nustotal_preprocessed.csv')
+courses_df.to_csv('../data/preprocessed.csv')
+
 
